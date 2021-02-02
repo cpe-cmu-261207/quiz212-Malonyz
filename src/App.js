9c4e0436cac06771ff0c6df5d589352d1b1be936
Malonyz <59277885+Malonyz@users.noreply.github.com>
2021-02-02 13:58:32 +0700

    gg

--- src/App.js
+++ src/App.js
@@ -11,19 +11,36 @@ function App() {
       age: "20"
     }
   ];
+  for (let i = 0; i < persons; i++) {
+    return persons(i);
+  }
+  let counter = 0;
+  const countUp = () => {
+    counter++;
+    document.querySelector("p").innerHTML = counter;
+  };
+  const countDown = () => {
+    counter--;
+    document.querySelator("p").innerHTML = counter;
+  };
+
   return (
     <div class="ml-2">
       {/* Code me please! */}
+
       <div class="mb-4">
         <h3 class="title is-3">ID Counter</h3>
-        <p>YOUR ID HERE</p>
-        <button>-</button>
+        <p>620612151</p>
+        <button onClick="countDown">-</button>
         <button>reset</button>
-        <button>+</button>
+        <button onclick="countUp">+</button>
       </div>
 
       {/* Convert me to a component! */}
+
       <h3 class="title is-3">Person List</h3>
+      <table></table>
+
       <table class="table is-bordered mb-3">
         <tbody>
           <tr>
@@ -35,6 +52,20 @@ function App() {
             <td>Bob</td>
             <td>male</td>
             <td>50</td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="table is-bordered mb-3">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Age</th>
+          </tr>
+          <tr>
+            <td>Alice</td>
+            <td>female</td>
+            <td>20</td>
           </tr>
         </tbody>
       </table>
